66fde11d249317dcbaf34a1233b945f88bd467cc
Silver Lumi <lumisilver@gmail.com>
2019-03-17 20:20:48 +0000

    add error handling

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     };
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -58,7 +59,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
-    .catch(error => error);
+    .catch(error => this.setState({ error }));
   }
 
   componentDidMount() {
@@ -78,7 +79,7 @@ class App extends Component {
     if (this.needsToSearchTopStories(searchTerm)) {
       this.fetchSearchTopStories(searchTerm);
     }
-    
+
     event.preventDefault();
   }
 
@@ -101,7 +102,8 @@ class App extends Component {
     const { 
       searchTerm, 
       results, 
-      searchKey 
+      searchKey,
+      error 
     } = this.state;
 
     const page = (
@@ -127,10 +129,15 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table 
-          list={list}
-          onDismiss={this.onDismiss}
-        />
+        { error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+            </div>
+          : <Table 
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
@@ -157,7 +164,7 @@ const Search = ({ value, onChange, onSubmit, children }) => {
 }
 
 
-const Table = ({ list, pattern, onDismiss }) => {
+const Table = ({ list, onDismiss }) => {
   const largeColumn = {
     width: '40%',
   };
